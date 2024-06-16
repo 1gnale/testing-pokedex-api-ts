5f69e33d0d4a8667a5ccff967a701a6e9f28c818
Ignale <nosoyignacio@correo.com>
2024-06-16 08:38:59 -0300

    feat: add get pokemon by name route

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,17 +4,17 @@ import { Types, Pokemon, PokemonList } from '../types';
 import { PokemonModel, TypeModel } from '../db';
 
 
-const { URL_API_TYPES, URL_API} = process.env as {
+const { URL_API_TYPES, URL_API } = process.env as {
     URL_API_TYPES: string;
     URL_API: string;
-  };
+};
 
-export const getAllTypes = async (): Promise<{name: Types}[]> => {
+export const getAllTypes = async (): Promise<{ name: Types }[]> => {
     const response = await fetch(URL_API_TYPES);
     const data = await response.json();
     const allTypes = data.results;
 
-    const cleanTypes: {name: Types}[] = allTypes.map((type: { name: string }) => {
+    const cleanTypes: { name: Types }[] = allTypes.map((type: { name: string }) => {
         return {
             name: type.name
         }
@@ -23,19 +23,19 @@ export const getAllTypes = async (): Promise<{name: Types}[]> => {
     return cleanTypes;
 }
 
- const getPokemonsDB = async (): Promise<Pokemon[]> => {
-     const dbPokemons = await PokemonModel.findAll({
-         include: {
-             model: TypeModel,
-             attributes: ['name'],
-             through: {
-                 attributes: []
-             }
-         }
-     });
+const getPokemonsDB = async (): Promise<Pokemon[]> => {
+    const dbPokemons = await PokemonModel.findAll({
+        include: {
+            model: TypeModel,
+            attributes: ['name'],
+            through: {
+                attributes: []
+            }
+        }
+    });
 
-     const dbPokemonsMapped = dbPokemons.map(pokemon => {
-         return {
+    const dbPokemonsMapped = dbPokemons.map(pokemon => {
+        return {
             id: pokemon.getDataValue('id'),
             name: pokemon.getDataValue('name'),
             hp: pokemon.getDataValue('hp'),
@@ -46,30 +46,37 @@ export const getAllTypes = async (): Promise<{name: Types}[]> => {
             weight: pokemon.getDataValue('weight'),
             img: pokemon.getDataValue('img'),
             types: pokemon.getDataValue('types').map((e: any) => e.name)
-         }
-     });
+        }
+    });
 
-     return dbPokemonsMapped;
- }
+    return dbPokemonsMapped;
+}
 
- const getPokemonsApi = async (): Promise<PokemonList[]> => {
-     const apiPokemons = await fetch(URL_API)
-         .then(res => res.json())
-         .then(data => data.results);
+const getPokemonsApi = async (): Promise<PokemonList[]> => {
+    const apiPokemons = await fetch(URL_API)
+        .then(res => res.json())
+        .then(data => data.results);
 
-     const apiPokemonsMapped: PokemonList[] = apiPokemons.map((pokemon: { name: string, url: string }) => {
-         return {
-             name: pokemon.name,
-             id: pokemon.url.split('/')[6]
-         }
-     });
+    const apiPokemonsMapped: PokemonList[] = apiPokemons.map((pokemon: { name: string, url: string }) => {
+        return {
+            name: pokemon.name,
+            id: pokemon.url.split('/')[6]
+        }
+    });
 
-     return apiPokemonsMapped;
- }
+    return apiPokemonsMapped;
+}
 
- export const getAllPokemons = async (): Promise<(PokemonList | Pokemon)[]> => {
-     const dbPokemons = await getPokemonsDB();
-     const apiPokemons = await getPokemonsApi();
+export const getAllPokemons = async (): Promise<(PokemonList | Pokemon)[]> => {
+    const dbPokemons = await getPokemonsDB();
+    const apiPokemons = await getPokemonsApi();
 
-     return [...dbPokemons, ...apiPokemons];
- }
+    return [...dbPokemons, ...apiPokemons];
+}
+
+export const getPokemonByName = async (name: string): Promise<PokemonList[]> => {
+    const allPokemons = await getAllPokemons();
+    const searchedPokemon = allPokemons.filter((pokemon) => pokemon.name.toLowerCase().includes(name.toLowerCase()));
+
+    return searchedPokemon;
+}
