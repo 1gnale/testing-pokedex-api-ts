189979e42540da6f33cb652f809cca0617e117e6
Ignale <nosoyignacio@correo.com>
2024-06-16 09:18:03 -0300

    feat: add get pokemon by id route

--- src/routes/pokemonsRouter.ts
+++ src/routes/pokemonsRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import getPokemonsController from "../controllers/getPokemonsController";
 import getPokemonByNameController  from "../controllers/getPokemonByNameController";
+import getPokemonByIdController from "../controllers/getPokemonByIdController";
 
 const pokemonsRouter = Router();
 
 pokemonsRouter.get("/", getPokemonByNameController , getPokemonsController)
+
+pokemonsRouter.get("/:id", getPokemonByIdController)
 
 export default pokemonsRouter;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,9 +4,10 @@ import { Types, Pokemon, PokemonList } from '../types';
 import { PokemonModel, TypeModel } from '../db';
 
 
-const { URL_API_TYPES, URL_API } = process.env as {
+const { URL_API_TYPES, URL_API, URL_API_ID } = process.env as {
     URL_API_TYPES: string;
     URL_API: string;
+    URL_API_ID: string;
 };
 
 export const getAllTypes = async (): Promise<{ name: Types }[]> => {
@@ -78,4 +79,22 @@ export const getPokemonByName = async (name: string): Promise<PokemonList[]> => 
     const searchedPokemon = allPokemons.filter((pokemon) => pokemon.name.toLowerCase().includes(name.toLowerCase()));
 
     return searchedPokemon;
+}
+
+export const getPokemonById = async (id: string): Promise<Pokemon> => {
+    const response = await fetch(`${URL_API_ID}/${id}`);
+    const data = await response.json();
+    const filtredPokemon : Pokemon = {
+        id: data.id,
+        name: data.name,
+        hp: data.stats[0].base_stat,
+        str: data.stats[1].base_stat,
+        def: data.stats[2].base_stat,
+        spd: data.stats[5].base_stat,
+        height: data.height,
+        weight: data.weight,
+        img: data.sprites.other['official-artwork'].front_default,
+        types: data.types.map((type: { type: { name: string } }) => type.type.name)
+    }
+    return filtredPokemon;
 }
